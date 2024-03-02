92d4c84ab78beb4965b7aeb504983ac8678f2cd5
Arslan <92268105+sollex551@users.noreply.github.com>
2024-03-02 19:05:03 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,19 +16,19 @@ async def check_subscription(message: Message):
     full_name = message.from_user.full_name
 
     await bot.send_message(message.from_user.id,
-                           f'Здравствуйте {full_name}\nВас приветствует  Чат гпт-бот. Подпишись на @AllApkaa ')
+                           f'Здравствуйте {full_name}\nВас приветствует  Чат гпт-бот. ')
 
 
 @dp.message_handler(commands=['help'])
 async def command_start(message: Message):
     full_name = message.from_user.full_name
-    await message.answer(f'Обратитесь в тех поддержку для помощи  (@CHAT_GPTADMIN)')
+    await message.answer(f'Обратитесь в тех поддержку для помощи  (@)')
 
 
 @dp.message_handler(content_types=['text'])
 async def handle_message(message: Message):
     chat_id = message.chat.id
-    await bot.send_message(chat_id, 'пожалуйста подпишись на мой канал @AllApkaa')
+    await bot.send_message(chat_id, 'Нейросеть задумалась')
 
     response = openai.Completion.create(
         engine="text-davinci-003",
